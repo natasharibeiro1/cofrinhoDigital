90763b72cd8efd86c0d037a66cb5a996aed63f64
natasharibeiro1 <natasha.ribeiro@ifam.edu.br>
2025-08-22 11:37:16 -0400

    Add files via upload

---
+++ ProjetoInfo3/app/src/main/java/com/example/projetoinfo3/splash/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.example.projetoinfo3.splash
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.projetoinfo3.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+    }
+}
